898cebb21b1087897a83b3aeae1936f432350b5f
ajeeshmanikandan <ajeesh.manikandan@techpearl.com>
2025-05-16 12:25:05 +0530

    chore: updated the log

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    console.log('CI/CD Setup ----->');
+    
+  }, [])
   return (
     <>
       <div>
